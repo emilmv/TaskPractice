683d88b42da36b52a853853ba5ca10ecf5c35835
emilmv <emilnm@code.edu.az>
2024-03-13 17:56:25 +0400

    minor improvements needed

--- TaskPractice/Program.cs
+++ TaskPractice/Program.cs
@@ -7,55 +7,62 @@ namespace TaskPractice
     {
         static void Main(string[] args)
         {
-        inputUsername:
-            Console.WriteLine(Role.Admin.ToString());
-            Console.WriteLine(ResponseMessage.UsernameInput);
-            Console.WriteLine(ResponseMessage.Divider);
-            string? username = Console.ReadLine();
-            if (string.IsNullOrEmpty(username))
-            {
-                Console.WriteLine(ResponseMessage.NullReferenceException);
-                Console.WriteLine(ResponseMessage.Divider);
-                goto inputUsername;
-            }
-        inputEmail:
-            Console.WriteLine(ResponseMessage.EmailInput);
-            Console.WriteLine(ResponseMessage.Divider);
-            string? email = Console.ReadLine();
-            if (string.IsNullOrEmpty(email))
-            {
-                Console.WriteLine(ResponseMessage.NullReferenceException);
-                Console.WriteLine(ResponseMessage.Divider);
-                goto inputEmail;
-            }
-        roleInput:
-            Console.WriteLine(ResponseMessage.RoleInput);
-            Console.WriteLine(ResponseMessage.Divider);
-            Console.WriteLine("Press 1 to choose Admin");
-            Console.WriteLine("Press 2 to choose Member");
-            bool isCorrectInput = int.TryParse(Console.ReadLine(), out int result);
-            if (!isCorrectInput)
-            {
-                Console.WriteLine(ResponseMessage.WrongInput);
-                goto roleInput;
-            }
-            switch (result)
-            {
-                case 1:
-                    User admin = new(username, email, Role.Admin);
-                    Console.WriteLine(ResponseMessage.Divider);
-                    admin.ShowInfo();
-                    break;
-                case 2:
-                    User member = new(username, email, Role.Member);
-                    Console.WriteLine(ResponseMessage.Divider);
-                    member.ShowInfo();
-                    break;
-                default:
-                    goto roleInput;
-            }
+            //inputUsername:
+            //    Console.WriteLine(Role.Admin.ToString());
+            //    Console.WriteLine(ResponseMessage.UsernameInput);
+            //    Console.WriteLine(ResponseMessage.Divider);
+            //    string? username = Console.ReadLine();
+            //    if (string.IsNullOrEmpty(username))
+            //    {
+            //        Console.WriteLine(ResponseMessage.NullReferenceException);
+            //        Console.WriteLine(ResponseMessage.Divider);
+            //        goto inputUsername;
+            //    }
+            //inputEmail:
+            //    Console.WriteLine(ResponseMessage.EmailInput);
+            //    Console.WriteLine(ResponseMessage.Divider);
+            //    string? email = Console.ReadLine();
+            //    if (string.IsNullOrEmpty(email))
+            //    {
+            //        Console.WriteLine(ResponseMessage.NullReferenceException);
+            //        Console.WriteLine(ResponseMessage.Divider);
+            //        goto inputEmail;
+            //    }
+            //roleInput:
+            //    Console.WriteLine(ResponseMessage.RoleInput);
+            //    Console.WriteLine(ResponseMessage.Divider);
+            //    Console.WriteLine("Press 1 to choose Admin");
+            //    Console.WriteLine("Press 2 to choose Member");
+            //    bool isCorrectInput = int.TryParse(Console.ReadLine(), out int result);
+            //    if (!isCorrectInput)
+            //    {
+            //        Console.WriteLine(ResponseMessage.WrongInput);
+            //        goto roleInput;
+            //    }
+            //    switch (result)
+            //    {
+            //        case 1:
+            //            User admin = new(username, email, Role.Admin);
+            //            Console.WriteLine(ResponseMessage.Divider);
+            //            admin.ShowInfo();
+            //            break;
+            //        case 2:
+            //            User member = new(username, email, Role.Member);
+            //            Console.WriteLine(ResponseMessage.Divider);
+            //            member.ShowInfo();
+            //            break;
+            //        default:
+            //            goto roleInput;
+            //   }
 
-
+            Book book1 = new("Kitab1", "Author1", 500);
+            Console.WriteLine(book1.isDeleted);
+            Library library = new Library();
+            library.addBook(book1);
+            library.filterByPageCount(20, 1000);
+            library.editBookName(1, "Kitab3");
+            library.getBookById(1);
+            library.getAllBooks();
 
 
 
